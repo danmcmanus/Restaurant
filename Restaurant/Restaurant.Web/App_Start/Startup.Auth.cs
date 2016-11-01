a18efb66c1d6f3e6583c69cc183e3cd92e1fe6a7
dan mcmanus <dan.mcmanus@milwaukeetool.com>
2016-10-31 20:33:50 -0500

    Moved DBContext from web project to data project along with corresponding models

--- Restaurant/Restaurant.Web/App_Start/Startup.Auth.cs
+++ Restaurant/Restaurant.Web/App_Start/Startup.Auth.cs
@@ -5,7 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using Restaurant.Web.Models;
+using Restaurant.Data.Models;
 
 namespace Restaurant.Web
 {
@@ -34,7 +34,7 @@ namespace Restaurant.Web
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
